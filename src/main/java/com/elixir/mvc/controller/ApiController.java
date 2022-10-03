5f7df6cb3f94f58cc0deb3d044f8c86e1808ebb6
hyeonkkyu <milktea185ml@naver.com>
2022-10-03 17:40:20 +0900

    22.10.03 커밋

---
+++ src/main/java/com/elixir/mvc/controller/ApiController.java
@@ -0,0 +1,25 @@
+package com.elixir.mvc.controller;
+
+import java.util.Date;
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class ApiController {
+
+    @GetMapping("/test")
+    public String test() {
+        System.out.println("요청받음...");
+        return "테스트 성공입니다.";
+    }
+
+}
